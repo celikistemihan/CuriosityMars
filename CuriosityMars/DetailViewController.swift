21e33eb1c26dcfd540b501aebb68d4078f6508c2
istemihancelik <istemihancelik33@gmail.com>
2021-03-19 16:17:31 +0300

    ActivityViewController added, Info.plist updated to fullfil user privacy related issues

--- CuriosityMars/DetailViewController.swift
+++ CuriosityMars/DetailViewController.swift
@@ -13,17 +13,35 @@ class DetailViewController: UIViewController {
     var detailTitle: String?
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         navigationItem.largeTitleDisplayMode = .never
         title = detailTitle
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(share))
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
 
     }
+    @objc func share(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 1.0)
+        else {
+            print("No image exist")
+            return
+        }
+        
+        let viewController = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        //For iPad
+        viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(viewController, animated: true)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
